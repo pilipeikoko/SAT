b4b3bcfd7e2f7a2968c145716329402f9159a1bc
MksmOrlov <tawnhall8.5@gmail.com>
2022-03-30 21:33:23 +0300

    refactor main according to PEP8

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import nltk,random
+import nltk
+import random
+
+from nltk.corpus import treebank
+from nltk.corpus import names
+from nltk.corpus import wordnet as wn
+
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
@@ -9,15 +15,14 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('treebank')
 # in terminal pip3 install pandas
-from nltk.corpus import treebank
-from nltk.corpus import names
-from nltk.corpus import wordnet as wn
 
 sentence = "At eight o'clock on Thursday morning. Arthur didn't feel very good."
-#Tokenization
+
+# Tokenization
 tokens = nltk.word_tokenize(sentence)
 print(tokens)
-#POS Tag
+
+# POS Tag
 tagged = nltk.pos_tag(tokens)
 print(tagged[0:6])
 
@@ -30,12 +35,16 @@ print(entities)
 
 print(nltk.corpus.brown.tagged_words())
 
+
 # classification : Gender identification
 def gender_features(word):
     return {'last_letter': word[-1]}
+
+
 print(gender_features('Shrek'))
 
-labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
+labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in
+                                                                         names.words('female.txt')])
 random.shuffle(labeled_names)
 featuresets = [(gender_features(n), gender) for (n, gender) in labeled_names]
 train_set, test_set = featuresets[500:], featuresets[:500]
@@ -44,11 +53,11 @@ print(classifier.classify(gender_features('Neo')))
 print(classifier.classify(gender_features('Trinity')))
 print(nltk.classify.accuracy(classifier, test_set))
 
-#Wordnet
+# Wordnet
 print(wn.synsets('motorcar'))
 print(wn.synset('car.n.01').lemma_names())
 print(wn.synset('car.n.01').definition())
 
-# display a prase tree form corpus treebank
+# display a parse tree form corpus treebank
 t = treebank.parsed_sents('wsj_0001.mrg')[0]
-t.draw() # opens a new window. 
+t.draw()  # opens a new window.
